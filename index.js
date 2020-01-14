7cf24b11501f36c767cf56905bba9bd8d8c9e650
niklasbec <niklasbeckerr@gmail.com>
2020-01-14 16:40:23 +0100

    done

--- index.js
+++ index.js
@@ -11,3 +11,9 @@ const postRouter = require('./posts/posts-router')
 
 
 server.use('/api/posts', postRouter)
+
+server.use('*', (req, res) =>
+res.status(404).json({
+  status: 404,
+  message: 'No endpoint matches that URL.'
+}))
